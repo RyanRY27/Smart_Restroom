a09c41671e49c57620feffc51a1f46baf200835c
@RyanRY27 <magdalitaryan7@gmail.com>
2024-10-20 23:21:36 +0800

    shits

--- Smart_RestRoom/Resources/JS/login.js
+++ Smart_RestRoom/Resources/JS/login.js
@@ -1,20 +1,26 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
+    // Initialize tooltips
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
-});
 
-document.getElementById('togglePassword').addEventListener('click', function () {
-    var passwordInput = document.getElementById('<%= passwordTextBox.ClientID %>');
-    var icon = this.querySelector('i');
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        icon.classList.remove('fa-eye');
-        icon.classList.add('fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
-        icon.classList.remove('fa-eye-slash');
-        icon.classList.add('fa-eye');
+    // Password toggle functionality
+    var passwordInput = document.getElementById('passwordTextBox');
+    var togglePassword = document.getElementById('togglePassword');
+
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function () {
+            var icon = this.querySelector('i');
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        });
     }
 });
